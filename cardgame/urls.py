2787cac14a80b4b4f68b9dfefc0f503bac9821cc
Gyuri Kim <kgr3819@gmail.com>
2022-01-21 03:28:03 +0900

    [FEAT]login/sign/logout

--- cardgame/urls.py
+++ cardgame/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import *
-
-app_name = "cardgame"
+from . import views
 
 urlpatterns = [
-    # path('', function_name, name=""),
+    path('', views.main_ready, name = 'main_ready'),
+    path('main/', views.main, name = 'main'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cardgame'
+    'cardgame',
+
+    # #allauth #추가
+    # 'allauth',
+    # 'allauth.account',
+    # 'allauth.socialaccount',
+
+    # #provider #추가
+    # 'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,10 +125,35 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# AUTH_USER_MODEL = 'cardgame.User'
+# # AUTH_USER_MODEL = 'cardgame.User'
+
+# AUTHENTICATION_BACKENDS = (
+#     # 'allauth' specific authentication methods, such as login by e-mail
+#     'allauth.account.auth_backends.AuthenticationBackend',
+
+#     #Needed to login by username in Django admin, regardless of 'allauth'
+#     'django.contrib.auth.backends.ModelBackend',
+    
+# )
+
+# SITE_ID = 1
+# LOGIN_REDIRECT_URL = '/'
+
+# SOCIALACCOUNT_PROVIDERS = {
+#     'google': {
+#         'SCOPE': [
+#             'profile',
+#             'email',
+#         ],
+#         'AUTH_PARAMS': {
+#             'access_type': 'online',
+#         }
+#     }
+# }
