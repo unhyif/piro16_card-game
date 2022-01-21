9c0407e580673a14d32d50ccb0fbf15eda99d38b
Jihyun Kim <ang21icjh@gmail.com>
2022-01-22 08:38:09 +0900

    Final

--- cardgame/views.py
+++ cardgame/views.py
@@ -6,7 +6,7 @@ from .models import *
 from .forms import *
 from random import *
 
-def loginuser(request):
+def login_user(request): # 로그인 중인 Profile object
     return Profile.objects.get(user=request.user)
 
 
@@ -41,39 +41,40 @@ def logout(request):
     auth.logout(request)
     return redirect('cardgame:main_ready')
 
-def main_ready(request):
+def main_ready(request): # 로그인 안 됐을 때 홈화면
     return render(request, 'cardgame/main_ready.html')    
 
-def main(request):
+def main(request): # 로그인 됐을 때 홈화면
     return render(request, 'cardgame/main.html')
+
 
 
 # Game
 
-def attack(request):
+def attack(request): # 공격하기 누르면 Game object 생성
     if request.method == "POST":
-        rule_list = ["BIG", "SMALL"]
-        chosen_rule = choice(rule_list)
+        chosen_rule = choice(["BIG", "SMALL"])
 
-        game = Game(attacker=loginuser(request), rule=chosen_rule) # Game object 생성
-        form = AttackForm(request.POST, instance=game)
+        game = Game(attacker=login_user(request), rule=chosen_rule) # attacker가 login_user이고, 랜덤으로 choice된 rule을 가진 Game
+        form = AttackForm(request.POST, instance=game) # form에 해당하는 instance를 방금 만든 game으로 지정
         if form.is_valid():
-            form.save()
+            form.save() # form에 담긴 정보가 game에게 전달
             return redirect("cardgame:detail", game.id)
 
     else:
-        form = AttackForm()
-        form.fields["defender"].queryset = Profile.objects.all().exclude(user=request.user)
+        form = AttackForm() # attacker_num, defender만 field로 가진 form
+        form.fields["defender"].queryset = Profile.objects.all().exclude(user=request.user) # 자기 자신 제외한 유저 목록
         return render(request, "cardgame/attack.html", {"form":form})
 
 def detail(request, pk):
     game = get_object_or_404(Game, id=pk)
     
-    if loginuser(request) == game.attacker:
-        attacker_in = True # 로그인한 유저가 attacker
-    else:
-        attacker_in = False # 로그인한 유저가 defender
-    return render(request, "cardgame/detail.html", {"game":game, "attacker_in":attacker_in, "status":status(pk)})
+    # if login_user(request) == game.attacker:
+    #     attacker_in = True # 로그인한 유저가 attacker
+    # else:
+    #     attacker_in = False # 로그인한 유저가 defender
+    return render(request, "cardgame/detail.html", {"game":game, "login_user":login_user(request)})
+    # return render(request, "cardgame/detail.html", {"game":game, "attacker_in":attacker_in, "status":status(pk)})
 
 def delete(request, pk):
    game = get_object_or_404(Game, id=pk)
@@ -87,80 +88,73 @@ def defend(request, pk):
         form = DefendForm(request.POST, instance=game)
         if form.is_valid():
             form.save()
-            get_winner(game)
+            get_winner(game) # game의 모든 field 채워짐
             return redirect("cardgame:detail", pk=game.id)
 
     else:
-        form = DefendForm()
+        form = DefendForm() # defender_num만 field로 가진 form
         return render(request, "cardgame/defend.html", {"form":form})
 
-
 def get_winner(game):
-    if game.rule == 'BIG':
+    if game.rule == "BIG":
         if game.attacker_num > game.defender_num:
             game.winner = game.attacker
             game.attacker.score += game.attacker_num
             game.defender.score -= game.defender_num
-            game.save()
-            game.attacker.save()
-            game.defender.save()
+            # game.save()
+            # game.attacker.save()
+            # game.defender.save()
         
         elif game.attacker_num == game.defender_num:
             game.winner = None
-            game.save()
+            # game.save()
 
         else:
             game.winner = game.defender
             game.attacker.score -= game.attacker_num
             game.defender.score += game.defender_num
-            game.save()
-            game.attacker.save()
-            game.defender.save()
+            # game.save()
+            # game.attacker.save()
+            # game.defender.save()
 
     else:
         if game.attacker_num < game.defender_num:
             game.winner = game.attacker
             game.attacker.score += game.attacker_num
             game.defender.score -= game.defender_num
-            game.save()
-            game.attacker.save()
-            game.defender.save()
+            # game.save()
+            # game.attacker.save()
+            # game.defender.save()
         
         elif game.attacker_num == game.defender_num:
             game.winner = None
-            game.save()
+            # game.save()
 
         else:
             game.winner = game.defender
             game.attacker.score -= game.attacker_num
             game.defender.score += game.defender_num
-            game.save()
-            game.attacker.save()
-            game.defender.save()
+            # game.save()
+            # game.attacker.save()
+            # game.defender.save()
 
-
-def status(pk):
-    game = get_object_or_404(Game, id=pk)
-    
-    if not game.defender_num: # 공격만 한 상태
-        return 1
-    else: # 승패 난 상태
-        return 2
+    game.save()
+    game.attacker.save()
+    game.defender.save()
 
 def ranking(request):
     users = Profile.objects.all().order_by('-score')
     index = users.count
     ctx = {'users':users, 'index':index}
-
     return render(request, template_name='cardgame/ranking.html', context=ctx)
+
+def list(request):
+    related_games = login_user(request).attack.all().union(login_user(request).defend.all())
+    return render(request, "cardgame/list_copy.html", {"related_games":related_games, "login_user":login_user(request)})  
 
 
 # def list(request):
-#     related_games = loginuser(request).attack.all().union(loginuser(request).defend.all())
-#     return render(request, "cardgame/list.html", {"related_games":related_games, })  
-
-def list(request):
-    user=request.user
-    game = Game.objects.all()
-    ctx = {'game': game,'user': user}
-    return render(request, "cardgame/list.html", context=ctx)
+#     user=request.user
+#     game = Game.objects.all()
+#     ctx = {'game': game,'user': user}
+#     return render(request, "cardgame/list.html", context=ctx)
