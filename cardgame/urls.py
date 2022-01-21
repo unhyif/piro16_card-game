c404d1be0d75707e11f3c0f68c7df22360ff8936
Jihyun Kim <ang21icjh@gmail.com>
2022-01-21 18:53:02 +0900

    Delete

--- cardgame/urls.py
+++ cardgame/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('attack/', views.attack, name="attack"),
-    path('detail/<int:pk>/', views.detail, name="detail"),
+    path('attack/', views.attack, name='attack'),
+    #path('defend/<int:pk>', views.defend, name='defend'),
+    path('detail/<int:pk>/', views.detail, name='detail'),
+    #path('list/', views.list, name='list'),
+   path('delete/<int:pk>/', views.delete, name='delete'),
+    #path('ranking/', views.ranking, name='ranking'),
 ]
+
+
