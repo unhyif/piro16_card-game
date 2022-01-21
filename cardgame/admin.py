553fae8bd6dca007803a54edfe1ed4890a2d84de
Jihyun Kim <ang21icjh@gmail.com>
2022-01-22 00:30:39 +0900

    save 문제 해결

--- cardgame/admin.py
+++ cardgame/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-@admin.register(Game)
-class GameAdmin(admin.ModelAdmin):
-    list_display = ['attacker', 'defender', 'winner']
-    list_display_links = ['attacker', 'defender', 'winner']
-    search_fields = ['attacker__username', 'defender__username', 'winner__username']
+# @admin.register(Game)
+# class GameAdmin(admin.ModelAdmin):
+#     list_display = ['attacker', 'defender', 'winner']
+#     list_display_links = ['attacker', 'defender', 'winner']
+#     search_fields = ['attacker__user.username', 'defender__user.username', 'winner__user.username']
+admin.site.register(Profile)
+admin.site.register(Game)
--- cardgame/views.py
+++ cardgame/views.py
@@ -89,9 +89,8 @@ def defend(request, pk):
     if request.method == "POST":
         form = DefendForm(request.POST, instance=game)
         if form.is_valid():
+            form.save()
             game_win(game)
-            game = form.save()
-            
             return redirect("cardgame:detail", pk=game.id)
 
     else:
@@ -104,25 +103,43 @@ def game_win(game):
             game.winner = game.attacker
             game.attacker.score += game.attacker_num
             game.defender.score -= game.defender_num
+            game.save()
+            game.attacker.save()
+            game.defender.save()
         
         elif game.attacker_num == game.defender_num:
             game.winner = None
+            game.save()
+            game.attacker.save()
+            game.defender.save()
 
         else:
             game.winner = game.defender
             game.attacker.score -= game.attacker_num
             game.defender.score += game.defender_num
+            game.save()
+            game.attacker.save()
+            game.defender.save()
 
     else:
         if game.attacker_num < game.defender_num:
             game.winner = game.attacker
             game.attacker.score += game.attacker_num
             game.defender.score -= game.defender_num
+            game.save()
+            game.attacker.save()
+            game.defender.save()
         
         elif game.attacker_num == game.defender_num:
             game.winner = None
+            game.save()
+            game.attacker.save()
+            game.defender.save()
 
         else:
             game.winner = game.defender
             game.attacker.score -= game.attacker_num
             game.defender.score += game.defender_num
+            game.save()
+            game.attacker.save()
+            game.defender.save()
