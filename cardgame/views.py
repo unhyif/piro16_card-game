127b271ed13c2bfe605db4fa45c3537e7d382b97
gaeun <gaeun0204@gmail.com>
2022-01-21 20:30:16 +0900

    pull

--- cardgame/views.py
+++ cardgame/views.py
@@ -90,21 +90,17 @@ def game_win(pk):
         elif game.attacker_num == game.defender_num:
             game.winner = None
 
-        else:
-            game.winner = game.defender
-            game.attacker.score -= game.attacker_num
-            game.defender.score += game.defender_num
+   return redirect('cardgame:main')
+      # !!!!! list 페이지 만들고나서 redirect 수정하기
+def defend(request,pk):
+    game= get_object_or_404(Game, pk=pk)
+###########defend###########수정해야함 form을..
+    if request.method == "POST":
+        form = DefendForm(request.POST, instance=game)
+        if form.is_valid():
+            form.save()
+            return redirect("cardgame:detail", pk=game.pk) # 게임 디테일 보여줄 건지 전적 보여줄 건지?? game.id
 
+        form = DefendForm()
     else:
-        if game.attacker_num < game.defender_num:
-            game.winner = game.defender
-            game.attacker.score -= game.attacker_num
-            game.defender.score == game.defender_num
-        
-        elif game.attacker_num == game.defender_num:
-            game.winner = None
-
-        else:
-            game.winner = game.attacker
-            game.attacker.score += game.attacker_num
-            game.defender.score -= game.defender_num
+        return render(request, "cardgame/defend.html", {"form":form})
